fa4dfbebabb318e307475fd26eae3afa755743c4
noopy <young_ahn@yonsei.ac.kr>
2020-08-27 20:09:31 +0900

    change icon logo

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -12,7 +12,7 @@ module.exports = {
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
   },
-  icon: `content/assets/studying_snoopy.png`, // Add your favicon
+  icon: `content/assets/profile.png`, // Add your favicon
   keywords: [`연세대학교`, `교환학생`, `해외 교환`, `nlp`, `연세대`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
